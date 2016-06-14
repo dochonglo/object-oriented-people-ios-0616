9b62c2113263d2c65e2548b5186d15a130c8b575
dochonglo <ismaelbarry777@hotmail.com>
2016-06-14 11:36:25 -0400

    Object Oriented People Lab is Complete (All Tests Passed).

---
+++ ObjectOrientedPeople/FISPerson.h
@@ -0,0 +1,13 @@
+//
+//  FISPerson.h
+//  ObjectOrientedPeople
+//
+//  Created by Ismael Barry on 6/14/16.
+//  Copyright © 2016 al-tyus.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FISPerson : NSObject
+
+@end
---
+++ ObjectOrientedPeople/FISPerson.m
@@ -0,0 +1,13 @@
+//
+//  FISPerson.m
+//  ObjectOrientedPeople
+//
+//  Created by Ismael Barry on 6/14/16.
+//  Copyright © 2016 al-tyus.com. All rights reserved.
+//
+
+#import "FISPerson.h"
+
+@implementation FISPerson
+
+@end
